f3afdcfdab284b9a8fa9083cc360791b3abdb8b1
spielbrecher <43925182+spielbrecher@users.noreply.github.com>
2024-04-26 14:37:22 +0900

    Add files via upload

---
+++ ai.py
@@ -0,0 +1,69 @@
+from langchain.prompts import load_prompt
+from langchain.chat_models.gigachat import GigaChat
+import arxiv
+import config
+import pandas as pd
+
+credentials = 'YmZlNGMwYWQtM2E0ZS00NzQ3LWIzMzQtZWYxN2NjNTYxODEyOmIyMjkxZTI2LTg4NjktNDc1Yy05NjE5LTg2NzUxNzc3MWZmYg=='
+
+chat = GigaChat(credentials=credentials,scope='GIGACHAT_API_CORP', verify_ssl_certs=False)
+
+def create_hypothesis():
+    prompt = load_prompt("scientist_hypothesis.yaml")
+    chain = prompt | chat
+    hypothesis = chain.invoke(
+        {
+            "text": config.artickle_description
+        }
+    ).content
+    return hypothesis
+
+def create_ru_keywords():
+    prompt = load_prompt("scientist_keywords.yaml")
+    chain = prompt | chat
+    keywords = chain.invoke(
+        {
+            "text": config.artickle_description
+        }
+    ).content
+    return keywords
+
+def translate_keywords():
+    prompt = load_prompt("translation_rueng.yaml")
+    chain = prompt | chat
+    translated_keywords = chain.invoke(
+        {
+            "text": config.keywords
+        }
+    ).content
+    return translated_keywords
+
+def create_plan():
+    # Создаем план работы над статьей
+    prompt = load_prompt("scientist_plan.yaml")
+    chain = prompt | chat
+    plan = chain.invoke(
+        {
+            "text": config.artickle_description
+        }
+    ).content
+    return plan
+
+def get_literature():
+    # Construct the default API client.
+    client = arxiv.Client()
+
+    # Search for the most recent articles
+    search = arxiv.Search(
+        query=config.translated_keywords,
+        max_results=20,
+        sort_by=arxiv.SortCriterion.SubmittedDate
+    )
+
+    results = client.results(search)
+    all_results = list(results)
+    return all_results
+
+def save_pdf(num):
+    paper = config.literature[num]
+    paper.download_pdf(dirpath=".")
---
+++ config.py
@@ -0,0 +1,9 @@
+BOT_TOKEN = "6628699152:AAF3gMxFvymAqy9X5LXlPtqd5E-PeuWJj4s"
+artickle_name = ''
+artickle_synopsis = ''
+artickle_description = ''
+artickle_hypothesis = ''
+plan = ''
+keywords = ''
+translated_keywords = ''
+literature = []
